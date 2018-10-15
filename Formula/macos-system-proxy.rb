a2010764c3805d0e2c721a76422542d7afaefe19
Tony Lotts <tonylotts@users.noreply.github.com>
2018-10-16 00:37:54 +0800

    Installs v0.1.1

--- Formula/macos-system-proxy.rb
+++ Formula/macos-system-proxy.rb
@@ -1,8 +1,8 @@
 class MacosSystemProxy < Formula
   desc "Returns the proxy url for the given network service, and protocol, from macOS Network preferences."
   homepage "https://github.com/tonylotts/macos-system-proxy"
-  url "https://github.com/tonylotts/macos-system-proxy/archive/v0.1.0.tar.gz"
-  sha256 "181da2e8dc61e0e127e667e2835ec9a00f2b877f03bc6753fe03a7e997a0f302"
+  url "https://github.com/tonylotts/macos-system-proxy/archive/v0.1.1.tar.gz"
+  sha256 "a7fc2d6e4b4251402c67bc7496a04401e846150ea8ca2769c4ebf88f1b76eb2c"
 
   def install
     bin.install "bin/getproxy"
